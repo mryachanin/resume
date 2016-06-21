5550a0f465a914ed64013e58e8bad3f8ef1c7ef4
Michael Yachanin <michael@yachanin.com>
2016-06-21 00:56:37 -0400

    update: inline css in <head> to preserve @page at-rule and before selector

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp'),
-    inlineCSS = require('gulp-inline-css'),
+    inline = require('gulp-inline'),
     rename = require('gulp-rename'),
     htmlmin = require('gulp-htmlmin');
 
 gulp.task('default', function () {
-	return gulp.src('assets/resume.html')
-        .pipe(inlineCSS())
-        .pipe(htmlmin({collapseWhitespace: true}))
+    return gulp.src('assets/resume.html')
+        .pipe(inline({
+            base : 'assets/'
+        }))
+        .pipe(htmlmin({
+            collapseWhitespace : true,
+            minifyCSS : true
+        }))
         .pipe(rename('index.html'))
         .pipe(gulp.dest('dist'));
 });
