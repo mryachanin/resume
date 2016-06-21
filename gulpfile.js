7fd19f76184c35ebb62a32940edda00822f811c4
Michael Yachanin <michael@yachanin.com>
2016-06-21 00:15:08 -0400

    feat: minify html

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
     inlineCSS = require('gulp-inline-css'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    htmlmin = require('gulp-htmlmin');
 
 gulp.task('default', function () {
 	return gulp.src('assets/resume.html')
         .pipe(inlineCSS())
+        .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(rename('index.html'))
         .pipe(gulp.dest('dist'));
 });
